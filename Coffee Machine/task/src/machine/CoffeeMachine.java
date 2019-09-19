b4f7b90fa359b95cd081f99a7d48c3a26373df7e
Padraig O Cosgora <cozzyoc@gmail.com>
2019-09-19 23:05:07 +0100

    Upload 2

---
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -0,0 +1,17 @@
+package machine;
+
+import java.lang.ref.SoftReference;
+import java.sql.SQLOutput;
+
+public class CoffeeMachine {
+    public static void main(String[] args) {
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
+        System.out.println("Test 2");
+    }
+}
