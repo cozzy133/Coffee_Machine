43f23c28282182540857591cd41b1065449de445
Padraig O Cosgora <cozzyoc@gmail.com>
2019-09-19 23:17:14 +0100

    Gitkracken test

--- Coffee Machine/task/src/machine/CoffeeMachine.java
+++ Coffee Machine/task/src/machine/CoffeeMachine.java
@@ -13,6 +13,6 @@ public class CoffeeMachine {
         System.out.println("Pouring some milk into the cup");
         System.out.println("Coffee is ready!");
         System.out.println("Test 2");
-        System.out.println("This is a change test");
+        System.out.println("This is a change test345");
     }
 }
